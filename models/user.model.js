c7ff3864e716289e586eff8a30c1e85f3da2f625
Amaresh Maharana <amareshcoder@gmail.com>
2025-09-19 13:02:17 +0530

    fix: make salt field optional in user schema and add matchPassword static method

--- models/user.model.js
+++ models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new Schema(
   {
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    salt: { type: String, required: true },
+    salt: { type: String },
     password: { type: String, required: true },
     profileImgURL: {
       type: String,
@@ -28,6 +28,24 @@ userSchema.pre("save", function (next) {
   this.password = hashedPassword;
   this.salt = salt;
   next();
+});
+
+userSchema.static("matchPassword", async function (email, password) {
+  const user = await this.findOne({ email });
+
+  if (!user) throw new Error("User not found!!");
+
+  const salt = user.salt;
+  const hashedPassword = user.password;
+
+  const userProvidedHashedPassword = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  if (userProvidedHashedPassword !== hashedPassword)
+    throw new Error("Invalid password!!");
+
+  return user;
 });
 
 export const User = mongoose.model("User", userSchema);
