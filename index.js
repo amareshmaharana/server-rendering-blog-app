8d1c373b1f3f9436a2fa2e53d744399192165af9
Amaresh Maharana <amareshcoder@gmail.com>
2025-09-19 15:09:00 +0530

    feat: enhance middleware setup and improve home route rendering with user data

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 import { userRouter } from "./routes/user.route.js";
+import { checkForAuthenticationCookie } from "./middlewares/auth.middleware.js";
 
 const app = express();
 const PORT = 3000;
 
-// for database
+// {/* ++++++ DATABASE CONNECTION ++++++ */}
 mongoose
   .connect("mongodb://localhost:27017/blognode")
   .then((e) => console.log("DB successfully connected"))
   .catch((e) => console.log(e));
 
+// {/* ++++++ EJS SETUP ++++++ */}
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+// {/* ++++++ MIDDLEWARES ++++++ */}
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
+// {/* ++++++ ROUTES ++++++ */}
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    user: req.user,
+  });
 });
 
 app.use("/user", userRouter);
