9401284797c29a4ad7d82dba62c5df7a2e9b16ec
Amaresh Maharana <amareshcoder@gmail.com>
2025-09-20 13:58:59 +0530

    feat: implement blog routes for adding new blogs with file upload functionality

---
+++ routes/blog.route.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import multer from "multer";
+import path from "path";
+
+import { Blog } from "../models/Blog.model.js";
+
+export const blogRouter = Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads/`));
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()} - ${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+blogRouter.get("/add-new-blog", (req, res) => {
+  return res.render("addBlog", { user: req.user });
+});
+
+blogRouter.post("/", upload.single("coverImage"), async (req, res) => {
+  const { title, content } = req.body;
+  const blog = await Blog.create({
+    title,
+    body: content,
+    createdBy: req.user._id,
+    coverImageURL: `/uploads/${req.file.filename}`,
+  });
+  return res.redirect(`/blog/${blog._id}`);
+});
