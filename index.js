7603497b4b496cb6a837b0f30d679c4407cce1b0
Amaresh Maharana <amareshcoder@gmail.com>
2025-09-20 13:58:06 +0530

    feat: implement home route to fetch and display all blogs

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 
 import { userRouter } from "./routes/user.route.js";
 import { checkForAuthenticationCookie } from "./middlewares/auth.middleware.js";
+import { blogRouter } from "./routes/blog.route.js";
+import { Blog } from "./models/Blog.model.js";
 
 const app = express();
 const PORT = 3000;
@@ -23,15 +25,19 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
 // {/* ++++++ ROUTES ++++++ */}
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({}).sort({ createdAt: -1 });
   res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   });
 });
 
 app.use("/user", userRouter);
+app.use("/blog", blogRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT happily: ${PORT}`);
